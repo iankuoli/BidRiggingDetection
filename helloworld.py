34d32b631a0ad321b77d5b54d4091ff7c2e8597d
Wush Wu <wush978@gmail.com>
2015-02-14 15:20:11 +0800

    add two query cases

--- helloworld.py
+++ helloworld.py
@@ -5,12 +5,18 @@ import RelationshipEvaluator
 
 app = Flask("test")
 
-@app.route("/", methods=["POST"])
-def query_rig_bid_detection():
+@app.route("/bid/rigging/detection", methods=["POST"])
+def query_bid_rigging_detection():
 	req = request.get_json()
 	id = req["id"]
 	company_list = req["company"].split(",")
 	return("id: " + id + " company: " + ",".join(company_list))
+
+@app.route("/bid/rigging/list", methods=["POST"])
+	req = request.get_json()
+	id = req["id"]
+	return("id: " + id)
+	
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port="80", debug=False)
